87750d0380642826ed59514c5ef12ed637f143d5
FS <francesco9721@gmail.com>
2025-02-11 21:15:30 +0100

    neu

--- 08_beispiel_pres/script.js
+++ 08_beispiel_pres/script.js
@@ -1,63 +1,47 @@
-// Set up the dimensions of the chart
+// Ränder für das Diagramm definieren
 const margin = { top: 70, right: 40, bottom: 60, left: 175 }
 const width = 660 - margin.left - margin.right
 const height = 400 - margin.top - margin.bottom
 
-// Create the SVG container for the chart
+// Erstelle das SVG-Element für das Diagramm
 const svg = d3.select("#pizza-chart").append("svg")
   .attr("width", width + margin.left + margin.right)
   .attr("height", height + margin.top + margin.bottom)
   .append("g")
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-// Load and process the data
+//TODO - 1. Lade die Daten aus der Datei csv-Datei 
+//          und konvertiere die Werte in Zahlen
 d3.csv("pizza.csv").then(data => {
   data.forEach(d => {
-    d.total = +d.total;
+    d.statistik = +d.statistik;
   });
 
-  // Sort the data by total
+  //TODO - 2. Sortiere die Daten nach der Spalte statistik
   data.sort(function (a, b) {
-    return d3.ascending(a.total, b.total);
+    return d3.ascending(a.statistik, b.statistik);
   });
 
-  // Set the x and y scales
+  //TODO - 3. Definiere die Skalen für die x- und y-Achse
   const x = d3.scaleLinear()
     .range([0, width])
-    .domain([0, d3.max(data, function (d) { return d.total; })]);
+    .domain([0, d3.max(data, function (d) { return d.statistik; })]);
 
   const y = d3.scaleBand()
     .range([height, 0])
     .padding(0.1)
     .domain(data.map(function (d) { return d.pizza_adv; }));
 
-  // Create the x and y axes
+  // x- und y-Achsen erstellen
   const xAxis = d3.axisBottom(x)
     .ticks(5)
-    .tickSize(0); // remove ticks
 
   const yAxis = d3.axisLeft(y)
     .tickSize(0)
     .tickPadding(10);
 
 
-
-  // Add vertical gridlines
-//   svg.selectAll("line.vertical-grid")
-//     .data(x.ticks(5))
-//     .enter()
-//     .append("line")
-//     .attr("class", "vertical-grid")
-//     .attr("x1", function (d) { return x(d); })
-//     .attr("y1", 0)
-//     .attr("x2", function (d) { return x(d); })
-//     .attr("y2", height)
-//     .style("stroke", "gray")
-//     .style("stroke-width", 0.5)
-//     .style("stroke-dasharray", "3 3");
-
-
-  // Create the bars for the chart
+  //TODO - 5. Erstelle die Balken für das Diagramm
   svg.selectAll(".bar")
     .data(data)
     .enter().append("rect")
@@ -65,10 +49,10 @@ d3.csv("pizza.csv").then(data => {
     .attr("y", function (d) { return y(d.pizza_adv); })
     .attr("height", y.bandwidth())
     .attr("x", 0)
-    .attr("width", function (d) { return x(d.total); })
+    .attr("width", function (d) { return x(d.statistik); })
     .attr('fill', 'orange')
 
-  // Add the x and y axes to the chart
+  // X- und Y-Achsen hinzufügen
   svg.append("g")
     .attr("class", "x axis")
     .style("font-size", "10px")
@@ -89,23 +73,24 @@ d3.csv("pizza.csv").then(data => {
     });
 
 
-  // Add labels to the end of each bar
+  // Beschriftung der Balken hinzufügen
   svg.selectAll(".label")
     .data(data)
     .enter().append("text")
 
-    .attr("x", function (d) { return x(d.total) + 5; })
+    .attr("x", function (d) { return x(d.statistik) + 5; })
     .attr("y", function (d) { return y(d.pizza_adv) + y.bandwidth() / 2; })
     .attr("dy", ".35em")
 
-    // .style("font-family", "sans-serif")
-    // .style("font-size", "10px")
-    // .style("font-weight", "bold")
-    // .style('fill', '#3c3d28')
+    //TODO - 7. Füge Styles hinzu
+    .style("font-family", "sans-serif")
+    .style("font-size", "10px")
+    .style("font-weight", "bold")
+    .style('fill', '#3c3d28')
 
-    .text(function (d) { return d.total; });
+    .text(function (d) { return d.statistik; });
 
-  // Add Total label (Unten Titel mittig / optional)
+  // Add statistik label (Unten Titel mittig / optional)
 //   svg.append("text")
 //     .attr("transform", "translate(" + width / 2 + "," + (height + margin.bottom / 2) + ")")
 //     .style("text-anchor", "middle")
@@ -113,22 +98,25 @@ d3.csv("pizza.csv").then(data => {
 //     .style("fill", "black")
 //     .style("font-family", "sans-serif")
 //     .attr("dy", "1em")
-//     .text("Total");
+//     .text("statistik");
 
   // Add the chart title
   svg.append("text")
     .attr("x", margin.left - 335)
     .attr("y", margin.top - 110)
 
+    // TODO - 8. Füge Styles für den Titel hinzu
     .style("font-size", "14px")
     .style("font-weight", "bold")
     .style("font-family", "sans-serif")
 
+    // TODO - 10. Füge den Titel hinzu
     .text("Beliebteste Pizza an der ADV");
 
-  // Add the chart data source
+  
   svg.append("text")
     .attr("transform", "translate(" + (margin.left - 335) + "," + (height + margin.bottom - 10) + ")")
+    // TODO - 9. Füge Quelle hinzu
     // .style("text-anchor", "start")
     // .style("font-size", "8px")
     // .style("fill", "lightgray")
